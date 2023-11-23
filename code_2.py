9a96dbeaccede338eb900e549e0fc81eaffc068f
EgarJ <141117607+EgarJ@users.noreply.github.com>
2023-11-23 10:20:57 +0000

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,43 @@
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = generateRandomData();
+
+}
+
+main();
+function main() {
+}
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+    }
+    });
+    return data;
+        console.log("Random Number: " + item);
+function generateRandomData() {
+    const data = [];if __name__ == "__main__":
+def generate_random_data():
+
+
+        print(f"Random Number: {item}")
+def main():
+    data = generate_random_data()
+    main()
+    for item in data:
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+import random    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+def generate_random_data():
+import random
+
+
+
+    for item in data:
+def main():
