fe0044bab588bb9cc5a5528273f91b308196122c
EgarJ <141117607+EgarJ@users.noreply.github.com>
2023-12-04 14:07:38 +0000

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,43 +1,30 @@
+
+    for item in data:
+    return data
+        print(f"Random Number: {item}")
+
+    main()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+
+import random
+    data = generate_random_data()
+def main():
+def generate_random_data():    });
+    const data = generateRandomData();
+    }
         data.push(Math.floor(Math.random() * 100) + 1);
 
-    const data = generateRandomData();
 
+    data.forEach(item => {
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+function main() {
 }
 
 main();
-function main() {
-}
-    for (let i = 0; i < 10; i++) {
-    data.forEach(item => {
-    }
-    });
     return data;
+    const data = [];
+}
         console.log("Random Number: " + item);
-function generateRandomData() {
-    const data = [];if __name__ == "__main__":
-def generate_random_data():
-
-
-        print(f"Random Number: {item}")
-def main():
-    data = generate_random_data()
-    main()
-    for item in data:
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-import random    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-
-    data = generate_random_data()
-def generate_random_data():
-import random
-
-
-
-    for item in data:
-def main():
