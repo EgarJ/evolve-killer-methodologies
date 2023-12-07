d627b3d396ce4074617e995c561887f12dbe4be8
EgarJ <141117607+EgarJ@users.noreply.github.com>
2023-12-07 09:09:43 +0000

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,30 +1,26 @@
+        }
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        int[] data = new int[10];
+        }
+    }
+        for (int item : data) {
+public class RandomDataGenerator {
+
+        for (int i = 0; i < 10; i++) {
+}
+    public static void main(String[] args) {
+
+        print(f"Random Number: {item}")
+def main():
+    return data
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+    main()
+def generate_random_data():
+
 
     for item in data:
-    return data
-        print(f"Random Number: {item}")
-
-    main()
-
-    data = [random.randint(1, 100) for _ in range(10)]
 if __name__ == "__main__":
-
-import random
-    data = generate_random_data()
-def main():
-def generate_random_data():    });
-    const data = generateRandomData();
-    }
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-
-    data.forEach(item => {
-function generateRandomData() {
-    for (let i = 0; i < 10; i++) {
-function main() {
-}
-
-main();
-    return data;
-    const data = [];
-}
-        console.log("Random Number: " + item);
