f9d31a6d7ae7672bc02662b7333f610d2dc27c03
EgarJ <141117607+EgarJ@users.noreply.github.com>
2023-12-10 04:10:29 +0000

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,38 @@
+        for (int item : data) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+}
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        }
+public class RandomDataGenerator {
+
+    }import random
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+    for item in data:
+    return data
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+
+def main():
+if __name__ == "__main__":
+def generate_random_data():
+    for item in data:
+if __name__ == "__main__":
+import random
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def generate_random_data():
+
+    return data
+    main()
+        print(f"Random Number: {item}")
