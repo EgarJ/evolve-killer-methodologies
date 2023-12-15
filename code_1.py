ee8a0fa3b8fea9c0f0ee1e3c076a5573479903cb
EgarJ <141117607+EgarJ@users.noreply.github.com>
2023-12-15 18:10:06 +0000

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,38 +1,32 @@
-        for (int item : data) {
+    data.forEach(item => {
+function main() {
+    return data;
 
-            data[i] = (int) (Math.random() * 100) + 1;
-        int[] data = new int[10];
+
+
+main();
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
 }
-        }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
-    public static void main(String[] args) {
-        }
-public class RandomDataGenerator {
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+    }
+}
+    const data = generateRandomData();
+    });
+    const data = [];    const data = [];
+    for (let i = 0; i < 10; i++) {
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
 
-    }import random
+}
 
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-
-    for item in data:
-    return data
-        print(f"Random Number: {item}")
-
-    data = generate_random_data()
-
-def main():
-if __name__ == "__main__":
-def generate_random_data():
-    for item in data:
-if __name__ == "__main__":
-import random
-
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-def generate_random_data():
-
-    return data
-    main()
-        print(f"Random Number: {item}")
+    data.forEach(item => {
+function main() {
+    return data;
+function generateRandomData() {
+}
+    }
+    const data = generateRandomData();
+main();
+        console.log("Random Number: " + item);
